2bfe2d0bd4a4a321c105eca049e80e9847b81419
Luke Burgess <burgesl@gmail.com>
2016-02-18 13:12:11 -0600

    Update NewclassTest.java

--- Silly/NewclassTest.java
+++ Silly/NewclassTest.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class NewclassTest { 
+public class BranchMathTest { 
 	
 	//Incase Error cannot be given or known.
 	public static String ProgramError = "Unknown"; 
@@ -22,6 +22,7 @@ public class NewclassTest {
 		public branch(long val){
 		System.out.println(val);
 			long v = val;
+			int num = 0;
 			Value[7] = (byte)(v>>56);
 			if((0 == Value[7]) | (-1 == Value[7])){
 				for (int c = 0; 7 > c; c++){
@@ -29,7 +30,14 @@ public class NewclassTest {
 				}
 				Value[7] = (byte)0;
 			}else{
-				
+				if (v<0) v = 0L - v; 
+				for (num = 0; v > 0; num++) v = v >> 1; 
+				v = val;
+				num = num - 55; 
+				for (int c = 0; 7 > c; c++)
+					Value[(6-c)] = (byte)(((v>>num) >> (6 - c << 3)));
+
+				Value[7] = (byte)(num);
 			}
 		}
 		public branch(float val){
@@ -51,27 +59,27 @@ public class NewclassTest {
 			int n=0;
 			switch (val.getClass().getSimpleName()){ 
 			case "Byte":
-				val = val;
+				n = (int)val;
 			case "Short":
-				val = (short) val;
+				n = (int)val;
 			case "Integer":
-				val = (int) val;
+				n = (int)val;
 			case "Float":
-				n = (int) val;
-				DVal = (double) n;
-				LVal = (long) n;
+				n = (int)val;
+				DVal = (double)n;
+				LVal = (long)n;
 			break;
 			case "Double":		
-				DVal = (double) val;
-				LVal = (long) val;
+				DVal = (double)val;
+				LVal = (long)val;
 			break;
 			case "String":
 				DVal = Double.parseDouble((String)val);
-				LVal = (long) DVal;
+				LVal = (long)DVal;
 			break;
 			case "Long":
-				n = (int) val;
-				LVal = (long) n;
+				LVal = (long)val;
+				n = (int)LVal;
 			break;
 			default:
 			break;
@@ -122,9 +130,14 @@ public class NewclassTest {
 			for(int c = i[0]; 0 < c; c--)
 				Tens = Tens + "10^";
 
-			Tens = " * " + Tens +i[1];
-			if (0==i[1] & 2 > i[1]) Tens = "  ";
-			return GetVal() + Tens;
+			Tens = " * " + Tens + (i[1]+2);
+			//if (0==i[1] & 2 > i[1]) Tens = "  ";
+
+			long val = (GetVal())/TenPower(i[1]);
+			int branch = (Value[7]<<27)>>27;
+			if (0 < branch) val = (val)<<branch;
+			val = val / 100L;
+			return val + Tens;
 		}
 		public String DisplayRawBytes(){
 			String Vals = Value[7] + " ";
@@ -139,26 +152,29 @@ public class NewclassTest {
 			Val = Val | ((long)(Value[3] & 0xFF))<<24;
 			Val = Val | ((long)(Value[2] & 0xFF))<<16;
 			Val = Val | ((long)(Value[1] & 0xFF))<<8;
-			Val = Val | (long)(Value[0] & 0xFF);
+			Val = Val | (long)(0xFF & (Value[0]));
 			return Val;
 		}
-		public int[] GetTenPower(){
-			return TenPower(Branch(Value[7]));
+		public static int TenPower(int raised){
+			if (0 < raised){
+			raised = raised - 1;
+			return 10*TenPower(raised);
+			}else{
+			return 1;
+			}
 		}
-		public static int[] TenPower(int[] raised){
-			int to = raised[1];
-			int rase = raised[0];
-			return ToTenPow(rase, to);
+		public int[] GetTenPower(){
+			return ToTenPow(Branch(Value[7]));
 		}
 //301029995663981195213738894724493026768189881462108541310 Log 2 / Log 10
 //10000000000
-		public static int[] ToTenPow(int rase, int to){
-			int[] raised = new int[]{rase, (LogBaseTwo(to))};
-			if (0 < to){
-				long sol = 30103L * to;
+		public static int[] ToTenPow(int[] to){
+			int[] raised = new int[]{to[0], (LogBaseTwo(to[1]))};
+			if (0 < to[1]){
+				long sol = 30103L * to[1];
 				raised[1] = (int)(sol / 100000L);
 			}
-			if (rase > 1){
+			if (to[0] > 1){
 				raised[1] = raised[1] - 1;
 			}
 			return raised;
@@ -193,7 +209,7 @@ public class NewclassTest {
 				raised = 65536 - raised;
 			}
 			if (0 < branch){
-				to = branch << 2;
+				to = branch;
 			}
 			return new int[]{raised, to};
 		}
@@ -214,7 +230,7 @@ public class NewclassTest {
 		}
 	}
 	public static void main(String[] args) {
-		if(null == args || 2 > args.length){
+		if(null == args || 1 > args.length){
 			ProgramError = "Usage: Up [Action] [Values]";
 			System.out.println(ProgramError);
 			ProgramError = "Not enough initial input arguments";
@@ -229,15 +245,19 @@ public class NewclassTest {
 		
 		Object K = InLnth;
 		
-		for (int c = 7; (0 - 1) < c; c--){
-			ByteArray[c] = (byte)Integer.parseInt(args[c]);
-		}
-		long val = (long)Integer.parseInt(args[8]);
+		long val = Long.parseLong(args[0], 10);
 		branch Test = new branch(val);
-		//branch NewTest = new branch(ByteArray);
-
 		System.out.println(Test.Display());
 		System.out.println(Test.DisplayRawBytes());
+		Test = branch.isSq(val);
+		System.out.println(Test.Display());
+		System.out.println(Test.DisplayRawBytes());
+
+		//for (int c = 7; (0 - 1) < c; c--){
+		//	ByteArray[c] = (byte)Integer.parseInt(args[c]);
+		//}
+		//branch NewTest = new branch(ByteArray);
+
 		//System.out.println(NewTest.Display());
 		//System.out.println(NewTest.DisplayRawBytes());
 
